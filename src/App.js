a6b29558cbf42e36787d7eaa15965ca3a03345a9
kwenealete <kwenait@yahoo.ca>
2019-08-06 13:08:11 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class MealApp extends React.Component{
+  state = {
+    searchMealName : '',
+    meals: []
+  }
+
+  handleTextChange = (e) => {
+    this.setState({searchMealName:e.target.value})
+  }
+
+  handleSearch = () => {
+    this.getmeal(this.state.searchMealName)
+  }
+  
+  getmeal = (mealName) => {
+    let url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`
+
+    axios.get(url)
+    // .then(respo => respo.json)
+    .then(res => {
+      let data = res.data.meals
+      
+      this.setState({meals:data})     
+      
+    })
+  }
+
+  componentDidMount() {
+    this.getmeal()
+  }
+  render() {
+    
+    return (
+      <div>
+        <h1>Welcome to the meal search app</h1>
+        <input
+        onChange = {this.handleTextChange}
+        value = {this.state.searchMealName}
+        placeholder = 'enter meal name'
+        type = 'text'
+        name = 'text'
+          />
+        <button className='button' onClick = {this.handleSearch} > search </button> 
+          {this.state.meals ? (
+          <div id='meals-container'>
+          {this.state.meals.map((meal, index) => (
+          <div className='meal-class' key={index}>
+          <h1>{meal.strMeal}</h1>
+          <img src={meal.strMealThumb} alt="meal-thumbnail" />
+          </div>
+          ))}
+          </div>
+          ) : (
+          <p>Try searching for a meal</p>
+          )}
+          
+      </div>
+       
+    )
+  }
 }
 
-export default App;
+
+export default MealApp;
